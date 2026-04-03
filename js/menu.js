4bfe9efb470c3512d6c108900b36be4f4dce00ad
Kailane Lisley <108088775+KailaneLisley@users.noreply.github.com>
2026-04-03 19:27:15 +0000

    Adiciona seção "Sobre Mim" com informações pessoais e acadêmicas, atualiza o cabeçalho e ajusta estilos para melhor apresentação

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,13 @@
 document.addEventListener("componentsLoaded", () => {
 
+    const nameElement = document.querySelector(".name");
+
+    if (nameElement) {
+        nameElement.addEventListener("click", () => {
+            window.location.href = "index.html";
+        });
+    }
+
     const menuToggle = document.getElementById("menu-toggle");
     const navList = document.querySelector(".nav-list");
 
